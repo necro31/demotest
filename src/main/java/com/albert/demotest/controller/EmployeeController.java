13111916fefc3e3a62e3ff2feec2a72e71fae3af
Albert Kurniadinata <boy.albert21@gmail.com>
2023-09-17 14:31:19 +0700

    -app prop change url from db to localhost:40000 -chnage controller path

--- src/main/java/com/albert/demotest/controller/EmployeeController.java
+++ src/main/java/com/albert/demotest/controller/EmployeeController.java
@@ -3,33 +3,35 @@ package com.albert.demotest.controller;
 import com.albert.demotest.dto.CreateOrUpdateEmployee;
 import com.albert.demotest.dto.EmployeeDTO;
 import com.albert.demotest.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/employees")
+@RequestMapping("/gradle/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @Autowired
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
+//    @Autowired
+//    public EmployeeController(EmployeeService employeeService) {
+//        this.employeeService = employeeService;
+//    }
 
-    @PostMapping
+    @PostMapping("/create")
     public EmployeeDTO createNewEmployee(@RequestBody CreateOrUpdateEmployee createOrUpdateEmployee) {
         return employeeService.createNewEmployee(createOrUpdateEmployee);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public EmployeeDTO updateEmployeeById(@PathVariable Long id, @RequestBody CreateOrUpdateEmployee createOrUpdateEmployee) {
         return employeeService.updateEmployee(id, createOrUpdateEmployee);
     }
 
-    @GetMapping
+    @GetMapping("/get-all")
     public List<EmployeeDTO> getAllEmployee() {
         return employeeService.getAllEmployee();
     }
